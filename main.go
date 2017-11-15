ea98d0dbf2449f9ae31c18ea33e8a4c2d41b7c74
Frank Schmidt <rubiccuber@googlemail.com>
2017-11-16 00:24:28 +0100

    Add items to the list and refactor handlers

--- main.go
+++ main.go
@@ -12,40 +12,65 @@ var lists map[string]*list = make(map[string]*list)
 
 type list struct {
 	Id    string `json:"id,omitempty"`
-	Items string `json:"items,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Items []item `json:"items,omitempty"`
 }
 
-func ListHandler(w http.ResponseWriter, r *http.Request) {
+type item struct {
+	Name string
+}
+
+func CreateItem(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	if r.Method == "GET" {
-		l := lists[vars["id"]]
-		log.Print("GET /", vars["id"], "/ found:", l == nil)
-		if l == nil {
-			l = &list{}
-		}
+	l := lists[vars["id"]]
 
-		w.WriteHeader(400)
-		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-		json.NewEncoder(w).Encode(l)
-	} else if r.Method == "POST" {
-		var l list
-		if err := json.NewDecoder(r.Body).Decode(&l); err != nil {
-			log.Print("POST /", vars["id"], "/ json decode error ", err)
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		lists[l.Id] = &l
-		log.Print("POST /", vars["id"], "/ added:", l.Id)
-
-		w.WriteHeader(http.StatusCreated)
-		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	var i item
+	if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
+		log.Print("POST ", r.RequestURI, " jsonerr: ", err)
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
 	}
+	l.Items = append(l.Items, i)
+	log.Print("POST ", r.RequestURI, " itemadded: ", i)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+}
+func CreateList(w http.ResponseWriter, r *http.Request) {
+	var l list
+	if err := json.NewDecoder(r.Body).Decode(&l); err != nil {
+		log.Print("POST / jsonerr: ", err)
+		w.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+	lists[l.Id] = &l
+	log.Print("POST / listadded:", l.Id)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	json.NewEncoder(w).Encode(list{Id: l.Id})
+}
+func ViewList(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	l := lists[vars["id"]]
+	log.Print("GET /", vars["id"], "/ found:", l != nil)
+	if l == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	json.NewEncoder(w).Encode(l)
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/{id}/", ListHandler)
-	lists["abc"] = &list{"abc", "some items"}
+	r.StrictSlash(true)
+	r.HandleFunc("/", CreateList).Methods(http.MethodPost)
+	r.HandleFunc("/{id}/", ViewList).Methods(http.MethodGet)
+	r.HandleFunc("/{id}/", CreateItem).Methods(http.MethodPost)
+	lists["abc"] = &list{"abc", "some name", []item{}}
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
