74dad859cdd0df161f893006b5702176fc162687
Frank Schmidt <rubiccuber@googlemail.com>
2017-11-21 23:08:27 +0100

    Model basic web controlflow for list creation

--- web/server.go
+++ web/server.go
@@ -1,10 +1,17 @@
 package web
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+func renderPage(w http.ResponseWriter, name string, data interface{}) {
+	t := template.Must(template.ParseFiles("web/templates/basics.template", "web/templates/index.html", "web/templates/show.html"))
+	t.ExecuteTemplate(w, name, data)
+}
 
 func Run(r *mux.Router, prefix string) {
 	s := r.PathPrefix(prefix).Subrouter()
@@ -13,6 +20,11 @@ func Run(r *mux.Router, prefix string) {
 	)
 
 	s.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "hello world", http.StatusOK)
+		log.Print(r.Method + " " + r.URL.Path)
+		renderPage(w, "index.html", nil)
+	})
+	s.HandleFunc("/show/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print(r.Method + " " + r.URL.Path)
+		renderPage(w, "show.html", nil)
 	})
 }
